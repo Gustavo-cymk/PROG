2ebf0c8c8ca8da5fd3e2692ee3e8c070e37c58f1
GUSTAVO DA ROCHA ALVES <0030482611029@lti.fatecso.edu.br>
2026-09-22 12:29:50 -0300

    exerc aula terminei ate a b

---
+++ Biblioteca.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+typedef struct Livro{
+    int id;
+    char titulo[80];
+    char autor[60];
+    char categoria[40];
+    int ano;
+}livro;
+void receberLivros
+
+
+int main(){
+    livro v[5];
+    int n, i , ind;
+    char aux_categoria[40];
+
+
+}
--- conteudo de aula/Exercícios/Parte 1/pStruct1a.c
+++ conteudo de aula/Exercícios/Parte 1/pStruct1a.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 struct data { int dia, mes, ano;};
 
 struct medicao { int idEstacao;
@@ -7,33 +6,48 @@ struct medicao { int idEstacao;
 				 int pluviometria;
 				 float tempMaxima;
 				};
-				
+
 int main(void)
 {	struct medicao medicoes[10];
 	struct data dataInfo;
 	int cont;
-	
+
 	cont = 0;
-	while (cont < 3)//teste com 3 registros
+	while (cont < 1)//teste com 3 registros
 	{	printf("Informe registro #%02d\n", cont + 1);
 		printf("Estacao ....: ");
 		scanf("%d", &medicoes[cont].idEstacao);
 		printf("Data Medicao: ");
-		scanf("%d/%d/%d", &medicoes[cont].dataMedicao.dia, 
+		scanf("%d/%d/%d", &medicoes[cont].dataMedicao.dia,
 						  &medicoes[cont].dataMedicao.mes,
 						  &medicoes[cont].dataMedicao.ano);
 		printf("Pluviometria: ");
 		scanf("%d", &medicoes[cont].pluviometria);
 		printf("Temp. Maxima: ");
 		scanf("%f", &medicoes[cont].tempMaxima);
-		
+
 		cont = cont + 1;
 		printf("\n");
 	}
-	
+
 	printf("\n\n\n");
 	printf("Informe a data de interesse: ");
 	scanf("%d/%d/%d", &dataInfo.dia, &dataInfo.mes, &dataInfo.ano);
-	
+    for (int i = 0;i < cont ; i++){
+        if( medicoes[i].dataMedicao.dia == dataInfo.dia &&
+            medicoes[i].dataMedicao.mes == dataInfo.mes &&
+            medicoes[i].dataMedicao.ano == dataInfo.ano){
+                printf("Data:%d/%d/%d\n",dataInfo.dia,dataInfo.mes,dataInfo.ano);
+                printf("Estacao:%d\n",medicoes[i].idEstacao);
+                printf("Pluviometria:%d\n",medicoes[i].pluviometria);
+                printf("Temperatura Maxima:%.2f\n",medicoes[i].tempMaxima);
+        }
+    }
+    int somapluv = 0;
+    for(int i = 0;i<cont;i++){
+        somapluv += medicoes[i].pluviometria;
+    }
+    printf("Soma de pluviometria da estacoes:%d\n",somapluv);
+
 	return 0;
 }
